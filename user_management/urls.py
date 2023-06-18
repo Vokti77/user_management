ea020494bcbc9eff533cbab507c8a2ab9d25cfd9
Vokti77 <vokti77@gmail.com>
2023-06-18 17:23:20 +0600

    Reset & Change Password

---
+++ user_management/urls.py
@@ -0,0 +1,46 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from personal.views import (
+	home_screen_view
+)
+
+from account.views import (
+    register,
+    login_view,
+    logout_view,
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
+    path('register/', register, name='register'),
+    path('login/', login_view, name="login"), 
+    path('logout/', logout_view, name="logout"), 
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_reset/password_change.html'), 
+        name='password_change'),
+    
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset/password_change_done.html'), 
+        name='password_change_done'),
+
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset/password_reset_complete.html'),
+     name='password_reset_complete'),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
