ce8e4c62d8bfc943c2c47f89307fbcbec38a61af
Vokti77 <vokti77@gmail.com>
2023-06-19 13:59:20 +0600

    add account view

---
+++ User_Management/user_management/account/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.conf import settings
+
+from account.forms import RegistrationForm, AccountAuthenticationForm
+from account.models import Account
+
+def register(request, *args, **kwargs):
+	user = request.user
+	if user.is_authenticated: 
+		return HttpResponse("You are already authenticated as " + str(user.email))
+
+	context = {}
+	if request.POST:
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			email = form.cleaned_data.get('email').lower()
+			raw_password = form.cleaned_data.get('password1')
+			account = authenticate(email=email, password=raw_password)
+			login(request, account)
+			destination = kwargs.get("next")
+			if destination:
+				return redirect(destination)
+			return redirect('home')
+		else:
+			context['registration_form'] = form
+
+	else:
+		form = RegistrationForm()
+		context['registration_form'] = form
+	return render(request, 'account/registration.html', context)
+
+
+def logout_view(request):
+	logout(request)
+	return redirect("home")
+
+
+def login_view(request, *args, **kwargs):
+	context = {}
+
+	user = request.user
+	if user.is_authenticated: 
+		return redirect("home")
+
+	destination = get_redirect_if_exists(request)
+	print("destination: " + str(destination))
+
+	if request.POST:
+		form = AccountAuthenticationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email, password=password)
+
+			if user:
+				login(request, user)
+				if destination:
+					return redirect(destination)
+				return redirect("home")
+
+	else:
+		form = AccountAuthenticationForm()
+
+	context['login_form'] = form
+
+	return render(request, "account/login.html", context)
+
+
+def get_redirect_if_exists(request):
+	redirect = None
+	if request.GET:
+		if request.GET.get("next"):
+			redirect = str(request.GET.get("next"))
+	return redirect
+
+
+
+def account_view(request, *args, **kwargs):
+	"""
+	- Logic here is kind of tricky
+		is_self (boolean)
+			is_friend (boolean)
+				-1: NO_REQUEST_SENT
+				0: THEM_SENT_TO_YOU
+				1: YOU_SENT_TO_THEM
+	"""
+	context = {}
+	user_id = kwargs.get("user_id")
+	try:
+		account = Account.objects.get(pk=user_id)
+	except:
+		return HttpResponse("Something went wrong.")
+	if account:
+		context['id'] = account.id
+		context['username'] = account.username
+		context['email'] = account.email
+		context['profile_image'] = account.profile_image.url
+		context['hide_email'] = account.hide_email
+
+		# Define template variables
+		is_self = True
+		is_friend = False
+		user = request.user
+		if user.is_authenticated and user != account:
+			is_self = False
+		elif not user.is_authenticated:
+			is_self = False
+			
+		# Set the template variables to the values
+		context['is_self'] = is_self
+		context['is_friend'] = is_friend
+		context['BASE_URL'] = settings.BASE_URL
+		return render(request, "account/account.html", context)
